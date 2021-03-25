c9fcf5f75bec41b127aed65ad8c1e499ddbfbef3
Kylian Sauvee <kylian@sauvee.com>
2021-03-25 22:42:44 +0100

    read and print automaton information in file

--- src/automatons/Automaton.java
+++ src/automatons/Automaton.java
@@ -1,8 +1,35 @@
 package automatons;
 
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Automaton {
     private int nbStates;
     private LinkedList<State> states;
+
+    public void readAutomatonOnFile(final String filename) throws FileNotFoundException {
+        try {
+            File automatonInformation = new File(filename);
+            Scanner reader = new Scanner(automatonInformation);
+
+            for (int i = 0; i < 5; ++i) {
+                if (reader.hasNextLine()) {
+                    String data = reader.nextLine();
+                    System.out.println(data);
+                }
+            }
+
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error : File Not Found");
+            e.printStackTrace();
+        }
+    }
 }
