108f7cbe2b9c8dc98b2d1b6d7776e54e458d8225
Jules <jules.rubin@efrei.net>
2021-03-26 02:40:06 +0100

    determinisation v.0.1

--- src/automatons/Automaton.java
+++ src/automatons/Automaton.java
@@ -3,28 +3,112 @@ package automatons;
 import java.util.LinkedList;
 
 public class Automaton {
-	private Automaton next;
-    private int nbStates;
+    //private int nbStates; a.states.size() renvoie la taille de la liste d'états directement
     private LinkedList<State> states;
-    public static int s_alph; // [1;26]
+    public static int s_alph; // [1;26] et si mot vide [1;27]
     
     //Vous me suivez ? Vous confirmez ?
     
+    public Automaton(int nbStates, LinkedList<State> states) {
+		super();
+		//this.nbStates = nbStates;
+		this.states = states;
+	}
+
+	/*public int getNbStates() {
+		return nbStates;
+	}
+
+	public void setNbStates(int nbStates) {
+		this.nbStates = nbStates;
+	}*/
+
+	public LinkedList<State> getStates() {
+		return states;
+	}
+
+	public void setStates(LinkedList<State> states) {
+		this.states = states;
+	}
+
+	public static int getS_alph() {
+		return s_alph;
+	}
+
+	public static void setS_alph(int s_alph) {
+		Automaton.s_alph = s_alph;
+	}
     
     public boolean is_determinist(Automaton a) {
-    	int n=1;
-    	while(a.states != null) {
-    		
-    		a.states = (a.states).next;
-    		n++;
-    		while((this.neighbours).states != null) {
-    			
-    		}
-	    	if(n > s_alph) {
-	    		return false;
-	    	}
+		
+    	if(several_entries(a) && several_tr(a)) {
+    		System.out.println("L'automate n'est pas déterministe car il a plusieurs entrées "
+    				+ "et plusieurs transitions libellées par le même caractère");
+    		return false;
+    	}else if(several_entries(a)) {
+    		System.out.println("L'automate n'est pas déterministe car il a plusieurs entrées");
+    		return false;
+    	}else if(several_tr(a)) {
+    		System.out.println("L'automate n'est pas déterministe car il a "
+    				+ "plusieurs transitions libellées par le même caractère");
+    		return false;
     	}
-    	
     	return true;
     }
+    
+    public boolean several_entries(Automaton a) {
+    	int entries_cpt = 0;   //A ce stade l'automate a zero entrée
+    	boolean entries = false; // donc le booléen est initialisé à faux (pas plus d'une entrée)
+    	for(int i = 0; i < (a.states).size() ; i++) {
+    		if((a.states).get(i).isInit()) { // on compte les entrées
+    			entries_cpt++;
+    		}
+    		entries = (entries_cpt > 1) ? true : false;  // entries est supérieur à 1 ? Si oui true sinon false
+    	}
+    	return entries;
+    }
+    
+    public boolean several_tr(Automaton a) {
+    	for(int i = 0; i < (a.states).size() ; i++) {  //size() = méthode dans java.util qui donne la taille d'une LC
+    		if((((a.states).get(i)).getNeighbours()).size() > s_alph) {
+    			/*Si la liste des voisins est > à la taille de l'alphabet on a forcément plusieurs transitions pour un
+    			 * caractère donné à partir du i eme état -> l'automate est donc no  déterministe
+    			 * 
+    			 * idem get(i) donne le i eme element de la LC
+    			 * ni copie ni affectation juste on regarde la liste -> pas de modification -> pas besoin de copie sécur
+    			 */
+    			return true;
+    		}
+    	}
+		return false;
+    }
+
+	public Automaton det_sync(Automaton a){
+    	Automaton a_det = new Automaton(0, null);
+    	if(is_determinist(a)) { //si l'automate est déjà déterministe
+    		a_det = a;
+    	}else {
+    		LinkedList<State> new_states_list = new LinkedList<State>();
+			if(several_entries(a)) {
+				LinkedList<State> entries_list = new LinkedList<State>();
+				for(int i = 0; i < (a.states).size() ; i++) {
+					if((a.states).get(i).isInit()) {
+						entries_list.add(states.get(i));  // on récupère la liste des entrées
+					}
+				}
+				LinkedList<State> new_entries_list = (LinkedList<State>) entries_list.clone();
+				for(int i = 0 ; i < entries_list.size() ; i++) {
+					State new_entrie = new State(null, false, false, null);
+					new_entrie.concat_states(entries_list.get(i), entries_list.get(i+1));
+					new_entries_list.add(new_entrie);
+				}
+			}
+    	}
+    	
+    	return a_det;
+    }
+    
+    
+    
+    
 }
--- src/automatons/State.java
+++ src/automatons/State.java
@@ -1,10 +1,76 @@
 package automatons;
 
 import java.util.LinkedList;
+import java.util.Arrays;
 
 public class State {
     private String id;
     private boolean isInit;
     private boolean isExit;
     private LinkedList<String[]> neighbours;
+    
+    
+    public State(String id, boolean isInit, boolean isExit, LinkedList<String[]> neighbours) {
+		super();
+		this.id = id;
+		this.isInit = isInit;
+		this.isExit = isExit;
+		this.neighbours = neighbours;
+	}
+    
+	public LinkedList<String[]> getNeighbours() {
+		return neighbours;
+	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public boolean isInit() {
+		return isInit;
+	}
+
+	public void setInit(boolean isInit) {
+		this.isInit = isInit;
+	}
+
+	public boolean isExit() {
+		return isExit;
+	}
+
+	public void setExit(boolean isExit) {
+		this.isExit = isExit;
+	}
+
+	public void setNeighbours(LinkedList<String[]> neighbours) {
+		this.neighbours = neighbours;
+	}
+	
+	public void concat_states(State a, State b) {
+		LinkedList<String[]> new_neigh_list = new LinkedList<String[]>();  //création de la nouvelle liste de transitions depuis l'état donné
+		int cpt = 0;
+		for(int i = 0 ; i < (a.getNeighbours()).size() ; i++) {
+			for(int j = 0 ; j < (b.getNeighbours()).size();j++) {
+				if(a.getNeighbours().get(i)[0] == b.getNeighbours().get(i)[0]) {
+					String new_neigh;
+					if(a.getNeighbours().get(i)[1] == b.getNeighbours().get(i)[1]) {
+						new_neigh = a.getNeighbours().get(i)[0]+a.getNeighbours().get(i)[1];
+						
+					}else {
+						new_neigh = a.getNeighbours().get(i)[0]+a.getNeighbours().get(i)[1]+b.getNeighbours().get(i)[1];	
+					}
+					String[] new_neigh_array = new_neigh.split("");
+					new_neigh_list.add(new_neigh_array);
+				}
+			}
+		}
+		this.id = a.id+b.id;
+		this.isInit = a.isInit||b.isInit;
+		this.isExit = a.isExit||b.isExit;
+		this.neighbours = new_neigh_list;
+	}
+	
 }
