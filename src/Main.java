e474440dbc23a196817221472b05982fc00cfc57
Kylian Sauvee <kylian@sauvee.com>
2021-04-02 00:32:41 +0200

    add one automaton test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import automatons.Automaton;
+
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         // code here
+        LinkedList<String> automatonCharacteristics = Automaton.readAutomatonOnFile("./resources/A3-1.txt");
+        Automaton automaton = new Automaton(automatonCharacteristics);
+        Automaton stantardizedAutomaton = automaton.standardizedAutomaton();
     }
 }
