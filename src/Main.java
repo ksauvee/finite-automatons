0ea79d512f9ec39047f6a6ab4550a51815f40f57
Kylian Sauvee <kylian@sauvee.com>
2021-04-02 12:32:21 +0200

    remove tests

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,5 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         // code here
-        LinkedList<String> automatonCharacteristics = Automaton.readAutomatonOnFile("./resources/A3-1.txt");
-        Automaton automaton = new Automaton(automatonCharacteristics);
-        Automaton stantardizedAutomaton = automaton.standardizedAutomaton();
     }
 }
