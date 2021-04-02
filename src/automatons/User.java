46cb0c219d0c8124f5ad4b39294b02881f47ad12
Kylian Sauvee <kylian@sauvee.com>
2021-04-02 19:20:37 +0200

    add User class

---
+++ src/automatons/User.java
@@ -0,0 +1,20 @@
+package automatons;
+
+public class User {
+    private String word;
+    private String idInit;
+    private String idCurrentState;
+    private Automaton automatonTested;
+
+    public User(String idInit, Automaton automatonTested) {
+
+    }
+
+    public void readWord() {
+
+    }
+
+    public void isWordRecognizable() {
+
+    }
+}
