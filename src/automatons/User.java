6a8e9258896229a32aef0a74e3e862d3abbf8f77
Kylian Sauvee <kylian@sauvee.com>
2021-04-02 19:47:27 +0200

    User : add method isWordRecognizable

--- src/automatons/User.java
+++ src/automatons/User.java
@@ -1,11 +1,13 @@
 package automatons;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class User {
     private String word;
-    private final String idInit;
-    private final String idCurrentState;
+    private String idInit;
+    private String idCurrentState;
     private final Automaton automatonTested;
 
     public User(final String idInit, final Automaton automatonTested) {
@@ -16,11 +18,29 @@ public class User {
 
     public void readWord() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter word : (\"end\" stop the process of word recognition)");
+        System.out.println("Enter word (\"end\" stop the process of word recognition) : ");
         word = in.nextLine();
     }
 
-    public void isWordRecognizable() {
+    public boolean isWordRecognizable() {
+        int i = 0;
+        idCurrentState = idInit;
 
+        while (i < word.length()) {
+            char character = word.charAt(i);
+            State currentState = automatonTested.getStates().get(Integer.parseInt(idCurrentState));
+            HashMap<String, LinkedList<String>> currentStateNeighbours = currentState.getNeighbours();
+
+            // check case no possible transition
+            if (!currentStateNeighbours.containsKey(String.valueOf(character))) {
+                return false;
+            }
+
+            // get the next state
+            idCurrentState = currentStateNeighbours.get(String.valueOf(character)).get(0);
+            ++i;
+        }
+
+        return automatonTested.getStates().get(Integer.parseInt(idCurrentState)).getIsExit();
     }
 }
