7ac9138631605486d2e3989de3cd1bf23ea28929
kylian <kylian@sauvee.com>
2021-04-23 12:33:39 +0200

    add determinisation

---
+++ src/main/java/automatons/MyString.java
@@ -0,0 +1,41 @@
+package automatons;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class MyString {
+    private String str;
+
+    public MyString(String str) {
+        super();
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public String removeDuplicates() {//a corriger on split la string en tableau d id et on supprime les id en double
+        //remove the duplicates in the id of a state
+        String[] array = str.split("\\.");
+        LinkedList<String> list = new LinkedList<String>();
+        for(String number : array) {
+            list.add(number);
+        }
+        Collections.sort(list, Comparator.comparingInt((String s) -> s.length()).thenComparing((String s) -> s));
+        String result = "";
+        for (String number : list) {
+            result += number + ".";
+        }
+        if(result.length() > 0) {
+            result = result.substring(0,result.length()-1);
+        }
+
+        return result;
+    }
+}
